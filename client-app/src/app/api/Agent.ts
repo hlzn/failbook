04aba2b89daa1afe9aaa18e4de6ae8a7b01e802b
hugo.lozano@audatex.com <hugo.lozano@audatex.com>
2021-10-10 09:35:44 -0500

    changes for section 11

--- client-app/src/app/api/Agent.ts
+++ client-app/src/app/api/Agent.ts
@@ -63,7 +63,7 @@ const Activities = {
     list: () => request.get<Activity[]>('activities'),
     details: (id: string) => request.get<Activity>(`activities/${id}`),
     create: (activity: Activity) => axios.post<void>('activities', activity),
-    update: (activity: Activity) => axios.put<void>('activities', activity),
+    update: (activity: Activity) => axios.put<void>(`activities/${activity.id}`, activity),
     delete: (id: string) => axios.delete<void>(`activities/${id}`)
 }
 
